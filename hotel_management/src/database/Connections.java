26725aa9922930892112e6075bd54c43f966ff7b
Patricia Valerie Santoso <21xiepatrice@gmail.com>
2023-05-23 20:46:04 +0700

    .

--- hotel_management/src/database/Connections.java
+++ hotel_management/src/database/Connections.java
@@ -1,9 +1,21 @@
 package database;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+
 public class Connections {
+	public static Connection connect;
+	public static PreparedStatement state;
 
-	public Connections() {
-		// TODO Auto-generated constructor stub
+	public static void openCon() throws Exception {
+		Class.forName("com.mysql.cj.jdbc.Driver");
+		connect = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/carethree", "root", "");
 	}
-
+	
+	public static void closeCon() throws Exception{
+		state.close();
+		connect.close();
+	}
+	
 }
