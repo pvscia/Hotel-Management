8f919d22b30f7f849781b3176900a4e962ba6e6d
Colossies <127578135+Colossies@users.noreply.github.com>
2023-05-30 13:40:07 +0700

    Update Complaint.java
    Added room number and manual date insertion

--- hotel_management/src/complaints/Complaint.java
+++ hotel_management/src/complaints/Complaint.java
@@ -6,11 +6,20 @@ public class Complaint {
 	LocalDate date;
 	String complaint;
 	String id;
+	int roomNumber;
 	boolean resolved = false;
-	public Complaint(String id, String complaint){
+	public Complaint(int roomNumber, String id, String complaint){
+		this.roomNumber = roomNumber;
 		this.complaint = complaint;
 		this.id = id;
 		this.date = LocalDate.now();
+	}
+	
+	public Complaint(int roomNumber, String id, String complaint, int day, int month, int year) {
+		this.roomNumber = roomNumber;
+		this.complaint = complaint;
+		this.id = id;
+		date = LocalDate.of(year, month, day);
 	}
 	
 	public void setResolved() {
@@ -18,6 +27,6 @@ public class Complaint {
 	}
 	
 	public void printComplaint() {
-		System.out.println(id + ": " + complaint);
+		System.out.println("[" + date + "|" + roomNumber + "]" + id + ": " + complaint);
 	}
 }
