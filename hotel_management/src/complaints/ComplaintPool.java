7131ba6e8c8057658aa8f7f1f9493b246447e409
Colossies <127578135+Colossies@users.noreply.github.com>
2023-05-31 16:41:23 +0700

    Update ComplaintPool.java
    Changed to be a critique and rating system

--- hotel_management/src/complaints/ComplaintPool.java
+++ hotel_management/src/complaints/ComplaintPool.java
@@ -6,32 +6,69 @@ import java.time.*;
 import java.time.temporal.ChronoUnit;
 
 public class ComplaintPool {
+//	Complaint Pool is where complaints are stored
+//	Complaints: critique and suggestions
+	
 	ArrayList<Complaint> complaints = new ArrayList<Complaint>();
 	int totalComplaint = 0;
-	int resolvedComplaint = 0;
+	float avgRating;
 	
 	ComplaintPool(){
 		
 	}
 	
-	public void addComplaint(int roomNumber, String id, String complaint){
-//		Adds complaint with the date of entering the complaint as the date
+	public void updateRating() {
+//		Update the avgRating
+		int temp = 0;
+		for(int i = 0; i < totalComplaint; i++) {
+			temp += complaints.get(i).rating;
+		}
+		avgRating = temp / totalComplaint;
+	}
+	
+	
+	public void updateRatingFast(int rating) {
+//		For use after adding a complaint
+		float temp = avgRating * totalComplaint;
+		temp += rating;
+		avgRating = temp / (totalComplaint + 1);
+	}
+	
+	
+	public void updateRatingFastN(int rating) {
+//		For use after removing a complaint
+		float temp = avgRating * totalComplaint;
+		temp -= rating;
+		avgRating = temp / (totalComplaint - 1);
 		
-		complaints.add(new Complaint(roomNumber, id, complaint));
+	}
+	
+	public void getRating() {
+		System.out.println(avgRating);
+	}
+	
+	public void addComplaint(int roomNumber, String id, int rating, String complaint){
+//		Adds complaint with the date of entering the complaint as the date
+		if(rating < 1 || rating > 5) {
+			System.out.println("Rating must be within 1 and 5");
+			return;
+		}
+		complaints.add(new Complaint(roomNumber, id, rating, complaint));
+		updateRatingFast(rating);
 		totalComplaint++;
 	}
 	
-	public void addComplaintManual(int roomNumber, String id, String complaint, int day, int month, int year) {
+	public void addComplaintManual(int roomNumber, String id, int rating, String complaint, int day, int month, int year) {
 //		Adds complaint with a custom date
-		complaints.add(new Complaint(roomNumber, id, complaint, day, month, year));
+		if(rating < 1 || rating > 5) {
+			System.out.println("Rating must be within 1 and 5");
+			return;
+		}
+		complaints.add(new Complaint(roomNumber, id, rating, complaint, day, month, year));
+		updateRatingFast(rating);
 		totalComplaint++;
 	}
 	
-	public void setResolved(int index) {
-//		Set a complaint as resolved
-		complaints.get(index).setResolved();
-		resolvedComplaint++;
-	}
 	
 	public void viewAllComplaint() {
 		for(int i = 0; i < totalComplaint; i++) {
@@ -44,19 +81,9 @@ public class ComplaintPool {
 	}
 	
 	public void dropComplaint(int index) {
-		if(complaints.get(index).resolved) resolvedComplaint--;
+		updateRatingFastN(complaints.get(index).rating);
 		complaints.remove(index);
+		
 		totalComplaint--;
-		
-	}
-	
-	public void checkComplaintWeek() {
-//		Deletes all complaints older than 7 days
-		
-		for(int i = 0; i < totalComplaint; i++) {
-			LocalDate now = LocalDate.now();
-			long diff = ChronoUnit.DAYS.between(complaints.get(i).date, now);
-			if(diff > 7) dropComplaint(i);
-		}
 	}
 }
